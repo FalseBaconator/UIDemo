b63d6034ffb315d90bf3bc92dc450585f857e724
Ryan Wohler <ryanwohler55@gmail.com>
2023-09-27 14:42:46 -0300

    Invisible Wall to Gameplay

--- Assets/Scripts/PlayerNotSam.cs
+++ Assets/Scripts/PlayerNotSam.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerNotSam : MonoBehaviour
 {
 
     private GameManager gameManager;
-    private Vector3 spawn;
     public int HP;
     public int MaxHP;
+    private TextMeshProUGUI HPText;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindFirstObjectByType<GameManager>();
+        HPText = GameObject.FindGameObjectWithTag("HPText").GetComponent<TextMeshProUGUI>();
+        HPText.text = "HP: " + HP + "/" + MaxHP;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,7 +27,12 @@ public class PlayerNotSam : MonoBehaviour
                 gameManager.gameState = GameManager.GameState.Win;
                 break;
             case "Danger":
-                gameManager.gameState = GameManager.GameState.Lose;
+                HP--;
+                HPText.text = "HP: " + HP + "/" + MaxHP;
+                if (HP <= 0)
+                {
+                    gameManager.gameState = GameManager.GameState.Lose;
+                }
                 break;
         }
     }
