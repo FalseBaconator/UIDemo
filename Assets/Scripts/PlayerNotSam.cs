81672a5f6208d11ef23abeec53fd1950ee690981
Ryan Wohler <ryanwohler55@gmail.com>
2023-09-29 13:48:28 -0300

    Added Proper Respawn

--- Assets/Scripts/PlayerNotSam.cs
+++ Assets/Scripts/PlayerNotSam.cs
@@ -10,6 +10,7 @@ public class PlayerNotSam : MonoBehaviour
     public int HP;
     public int MaxHP;
     private TextMeshProUGUI HPText;
+    private Vector3 spawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,12 @@ public class PlayerNotSam : MonoBehaviour
         gameManager = FindFirstObjectByType<GameManager>();
         HPText = GameObject.FindGameObjectWithTag("HPText").GetComponent<TextMeshProUGUI>();
         HPText.text = "HP: " + HP + "/" + MaxHP;
+        spawnPoint = transform.position;
+    }
+
+    private void Update()
+    {
+        Debug.Log(spawnPoint);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,6 +39,12 @@ public class PlayerNotSam : MonoBehaviour
                 if (HP <= 0)
                 {
                     gameManager.gameState = GameManager.GameState.Lose;
+                }
+                else
+                {
+                    gameObject.GetComponent<CharacterController>().enabled = false;
+                    transform.position = spawnPoint;
+                    gameObject.GetComponent<CharacterController>().enabled = true;
                 }
                 break;
         }
