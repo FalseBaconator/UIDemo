75ceb8042fc80038657b9e39c58d717b8e079221
Ryan Wohler <ryanwohler55@gmail.com>
2023-11-08 14:26:29 -0400

    Updated Billboard

--- Assets/Scripts/PointAtCamera.cs
+++ Assets/Scripts/PointAtCamera.cs
@@ -14,6 +14,6 @@ public class PointAtCamera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.rotation = cam.transform.rotation;
+        transform.LookAt(transform.position + cam.transform.forward);
     }
 }
