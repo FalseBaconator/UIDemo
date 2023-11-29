7de611f5736f598957703cd0e14a227a45ec41ee
Ryan Wohler <ryanwohler55@gmail.com>
2023-11-29 15:10:23 -0400

    Added tweening to menu titles

--- Assets/Scripts/MenuItemAnim.cs
+++ Assets/Scripts/MenuItemAnim.cs
@@ -8,6 +8,8 @@ public class MenuItemAnim : MonoBehaviour
     public float duration;
     public bool rotation;
     public bool scale;
+    public bool fall;
+    public float fallHeight;
 
     private void OnEnable()
     {
@@ -19,6 +21,12 @@ public class MenuItemAnim : MonoBehaviour
         {
             transform.localScale = Vector3.zero;
             transform.DOScale(1, duration);
+        }
+        if (fall)
+        {
+            Vector3 pos = transform.position;
+            transform.position = new Vector3(transform.position.x, transform.position.y + fallHeight, transform.position.z);
+            transform.DOMove(pos, duration);
         }
     }
 
