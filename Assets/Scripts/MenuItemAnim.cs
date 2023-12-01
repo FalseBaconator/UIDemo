6f4317dca93681a1e772d10f574d76e251261ba3
Ryan Wohler <ryanwohler55@gmail.com>
2023-12-01 10:14:08 -0400

    Added easing and fixed bug

--- Assets/Scripts/MenuItemAnim.cs
+++ Assets/Scripts/MenuItemAnim.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuItemAnim : MonoBehaviour
 {
@@ -15,18 +16,24 @@ public class MenuItemAnim : MonoBehaviour
     {
         if (rotation)
         {
-            transform.DORotate(new Vector3(0, 0, 360), duration, RotateMode.FastBeyond360);
+            transform.DORotate(new Vector3(0, 0, 360), duration, RotateMode.FastBeyond360).SetEase(Ease.InOutSine).SetUpdate(true);
         }
         if (scale)
         {
             transform.localScale = Vector3.zero;
-            transform.DOScale(1, duration);
+            transform.DOScale(1, duration).SetEase(Ease.InOutSine).SetUpdate(true);
         }
         if (fall)
         {
-            Vector3 pos = transform.position;
-            transform.position = new Vector3(transform.position.x, transform.position.y + fallHeight, transform.position.z);
-            transform.DOMove(pos, duration);
+            //Vector3 pos = transform.position;   //Get where I want it to end
+            //transform.position = new Vector3(transform.position.x, transform.position.y + fallHeight, transform.position.z);    //Go where I want it to start
+            //transform.DOMove(pos, duration);    //Make it move
+
+            RectTransform rect = GetComponent<RectTransform>();
+            Vector2 pos = rect.anchoredPosition;
+            rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, rect.anchoredPosition.y + fallHeight);
+            DOTween.To(() => rect.anchoredPosition, x => rect.anchoredPosition = x, pos, duration).SetEase(Ease.OutBounce).SetUpdate(true);
+
         }
     }
 
