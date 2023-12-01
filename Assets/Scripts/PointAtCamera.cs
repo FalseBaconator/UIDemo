17f2ee64335bb31d48511d6d98ab9d7061826d3c
Ryan Wohler <ryanwohler55@gmail.com>
2023-12-01 14:15:08 -0400

    Added goal text bobbing

--- Assets/Scripts/PointAtCamera.cs
+++ Assets/Scripts/PointAtCamera.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,18 @@ public class PointAtCamera : MonoBehaviour
 {
     Camera cam;
 
+    public bool bob;
+    public float bobHeight;
+    public float bobDuration;
+
     private void Awake()
     {
         cam = Camera.main;
+        if (bob)
+        {
+            transform.DOMove(new Vector3(transform.position.x, transform.position.y + bobHeight, transform.position.z), bobDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+            //transform.DOJump(transform.position, bobHeight, 1, bobDuration).SetLoops(-1, LoopType.Restart);
+        }
     }
 
     // Update is called once per frame
